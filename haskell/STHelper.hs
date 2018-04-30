d03c8914d48aee00b634b4e2d7d8c0958e4042ef
Di Huang <di-huang@uiowa.edu>
2018-04-29 23:21:59 -0500

    Helper functions for the plc project

---
+++ haskell/STHelper.hs
@@ -0,0 +1,12 @@
+module SfxTree where
+
+-- get the tail part of a string using "tail [a]" function
+
+--whether  arg strA is the substring of arg strB
+isPrefix :: String -> String ->  Bool
+isPrefix "" strB = True
+isPrefix strA "" = False
+isPrefix (x:xs) (y:ys)
+  | x == y = isPrefix xs ys
+  | otherwise = False
+
